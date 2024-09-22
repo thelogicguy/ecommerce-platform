0bd4105df468ccec40025327f4a1edb16fd8410f
thelogicguy <107140254+thelogicguy@users.noreply.github.com>
2024-09-22 16:51:08 +0100

    update

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -8,13 +8,12 @@ export default [
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 2021, // Ensure this matches your code's features
-        sourceType: "module", // Use 'script' if not using modules
+        ecmaVersion: 2021, 
+        sourceType: "module",
       },
     },
     rules: {
-      "constructor-super": "off", // Disable this rule temporarily
-      // Add any other rules you want to customize here
+      "constructor-super": "off", 
     },
   },
   pluginJs.configs.recommended,

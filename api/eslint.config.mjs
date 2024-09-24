132c01a2b9262b87e34488c0573c20483ecbd0b3
thelogicguy <107140254+thelogicguy@users.noreply.github.com>
2024-09-24 09:16:32 +0100

    update

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -2,21 +2,10 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
+
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 2021, 
-        sourceType: "module",
-      },
-    },
-    rules: {
-      "constructor-super": "off", 
-    },
-  },
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
-
